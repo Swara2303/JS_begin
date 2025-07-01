1e137fb8f34ccbaf4a489e253cec88480e7d0a2e
Swara2303 <swarasagar2004@gmail.com>
2025-07-01 16:29:06 +0000

    conversions

---
+++ 01_basics /conversions.js
@@ -0,0 +1,41 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valInNumber = Number(score);
+// console.log(typeof valInNumber);
+// console.log(typeof score);
+// console.log(valInNumber);
+
+//"33" -> 33
+//"33abc" -> NaN
+//true -> 1 ; false -> 0;
+
+let isLoggedIn = null
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+//"Swara" -> true
+//"" -> false
+
+let someNumber = 33
+
+
+console.log(someNumber)
+console.log(typeof String(someNumber));
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
+
+
+
+
